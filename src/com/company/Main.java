51a5edabb35309413883f2aa9883acc60b47525d
Vahza <waja11@mail.ru>
2021-09-15 19:57:41 +0300

    secondNumbers

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i <1000 ; i=2*i-1) {
+            System.out.println("Число - "+ i);
+        }
+    }
+}
